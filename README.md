814e741627d320477dff9fb6b39931d21edcb41b
Lucas Cariddi <lucas.cariddi@compozelabs.com>
2022-09-26 18:54:49 +0200

    Adding Seed Module

---
+++ README.md
@@ -0,0 +1,79 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+<p align="center">A progressive <a href="http://nodejs.org" target="_blank">Node.js</a> framework for building efficient and scalable server-side applications.</p>
+<p align="center">
+
+## Description
+
+[Nest](https://github.com/nestjs/nest) framework TypeScript starter repository.
+
+## Installation
+
+```bash
+$ npm install
+```
+
+## Running in Development Mode (Yarn)
+
+1. Get the repo
+
+```bash
+$ git clone git@github.com:cariddi/Nest-Pokemon.git
+```
+
+2. Install dependencies
+
+```bash
+$ yarn install
+```
+
+3. Install Nest (globally)
+
+```bash
+$ npm i -g @nestjs/cli
+```
+
+4. Pull Mongo Image (Docker)
+
+```bash
+$  docker pull mongo
+```
+
+5. Start Docker Container (DB)
+
+```bash
+$  docker-compose up -d
+```
+
+6. Start the App
+
+```bash
+# development
+$ npm run start
+
+# watch mode
+$ npm run start:dev
+
+# production mode
+$ npm run start:prod
+```
+
+## Test
+
+```bash
+# unit tests
+$ npm run test
+
+# e2e tests
+$ npm run test:e2e
+
+# test coverage
+$ npm run test:cov
+```
+
+## Tech Stack
+
+- MongoDB
+- Nest
---
+++ src/pokemon/pokemon.service.ts
@@ -0,0 +1,93 @@
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { isValidObjectId, Model } from 'mongoose';
+import { NotFoundError } from 'rxjs';
+import { CreatePokemonDto } from './dto/create-pokemon.dto';
+import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Pokemon } from './entities/pokemon.entity';
+
+@Injectable()
+export class PokemonService {
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+  ) {}
+
+  async create(createPokemonDto: CreatePokemonDto) {
+    try {
+      return await this.pokemonModel.create(createPokemonDto);
+    } catch (error) {
+      this.handleExceptions(error);
+    }
+  }
+
+  findAll() {
+    return this.pokemonModel.find();
+  }
+
+  async findOne(term: string) {
+    let pokemonDB: Pokemon;
+
+    // No
+    if (!isNaN(+term)) {
+      pokemonDB = await this.pokemonModel.findOne({ no: term });
+    }
+
+    // Mongo ID
+    if (isValidObjectId(term) && !pokemonDB) {
+      pokemonDB = await this.pokemonModel.findById(term);
+    }
+
+    // Name
+    if (!pokemonDB) {
+      pokemonDB = await this.pokemonModel.findOne({ name: term });
+    }
+
+    if (!pokemonDB)
+      throw new BadRequestException(
+        `Pokemon with the following term ${term} not found`,
+      );
+
+    return pokemonDB;
+  }
+
+  async update(term: string, updatePokemonDto: UpdatePokemonDto) {
+    const pokemonDB: Pokemon = await this.findOne(term);
+
+    if (!pokemonDB)
+      throw new BadRequestException(
+        `Pokemon with the following term ${term} not found`,
+      );
+
+    try {
+      await pokemonDB.updateOne(updatePokemonDto);
+      return { ...pokemonDB.toJSON(), ...updatePokemonDto };
+    } catch (error) {
+      this.handleExceptions(error);
+    }
+  }
+
+  async remove(id: string) {
+    const result = await this.pokemonModel.deleteOne({ _id: id });
+    const { deletedCount } = result;
+
+    if (deletedCount === 0)
+      throw new BadRequestException(`Pokemon with id ${id} not found`);
+
+    return result;
+  }
+
+  private handleExceptions(error: any) {
+    if (error.code === 11000) {
+      throw new BadRequestException(
+        `Pokemon exists in DB ${JSON.stringify(error.keyValue)}`,
+      );
+    }
+    console.log(error);
+    throw new InternalServerErrorException(`Can't create pokemon - Check logs`);
+  }
+}
