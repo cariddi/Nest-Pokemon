fd57de10e4783eecbb3ce4871dd1814502e98996
Lucas Cariddi <lucas.cariddi@compozelabs.com>
2022-09-27 23:14:13 +0200

    Injecting http adapter dependency

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,23 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import axios, { AxiosInstance } from 'axios';
 import { Model } from 'mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { PokemonDB, PokeResponse } from './interfaces/poke-response.interface';
+import { AxiosAdapter } from '../common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
-  private readonly axios: AxiosInstance = axios;
-
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
   ) {}
 
   async excecuteSeed() {
     await this.pokemonModel.deleteMany({}); // equals to "delete * from <Table>"
 
-    const { data } = await this.axios.get<PokeResponse>(
+    const data = await this.http.get<PokeResponse>(
       'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
 
